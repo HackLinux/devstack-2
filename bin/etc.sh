ee0b869e099c89bf9e7cf27ac436231f4021589a
MIYATA Tadaaki <andes@flg.jp>
2015-04-18 19:01:41 +0900

    1.3

--- bin/etc.sh
+++ bin/etc.sh
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/bin/bash -x
 sudo cp ~/configs/interfaces /etc/network/interfaces
 sudo cp ~/configs/hostname /etc/hostname
@@ -6,9 +5,3 @@ sudo cp /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
 sudo apt-get -y install openssh-server openssh-server
 update.sh
 sudo reboot
-=======
-#!/bin/sh -x
-cp ~/configs/interfaces /etc/network/interfaces
-cp ~/configs/hostname /etc/hostname
-cp /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
->>>>>>> 739b53234efc876ef6b262a8e76cd3620763fe22
