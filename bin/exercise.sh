05b670759c8baa2392df10241ca3b05ba0e5c785
MIYATA Tadaaki <andes@flg.jp>
2015-04-18 19:00:38 +0900

    1.3

--- bin/exercise.sh
+++ bin/exercise.sh
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 #!/bin/bash -x
-=======
-#!/usr/bin/env bash
->>>>>>> 739b53234efc876ef6b262a8e76cd3620763fe22
 export ACTIVE_TIMEOUT=120
 export VOLUME_DELETE_TIMEOUT=120
 export VOLUME_TIMEOUT=120
